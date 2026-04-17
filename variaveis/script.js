f5fcea9dbcaa4fc2f36b927029c271bbb0d2685e
Ponton_Silva <raphaelrodriguesteste@gmail.com>
2026-04-17 18:52:57 -0300

    17/04

---
+++ variaveis/script.js
@@ -0,0 +1,25 @@
+"use strict";
+var carro = "Fusca";
+
+function mostrarCarro() {
+  console.log(carro);
+}
+
+mostrarCarro();
+console.log(carro);
+
+var mes;
+
+if (false) {
+  const mes = "Dezembro";
+  console.log(mes);
+}
+
+// console.log(mes);
+
+{
+  var carro = "Fusca";
+  const ano = 2018;
+}
+console.log(carro);
+console.log(ano);
