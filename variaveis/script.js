bb8afb7e491c18fa9ab81e9f70e2aae17afd4dd0
Ponton_Silva <raphaelrodriguesteste@gmail.com>
2026-04-20 18:33:45 -0300

    18:33

--- variaveis/script.js
+++ variaveis/script.js
@@ -1,25 +1,28 @@
-"use strict";
-var carro = "Fusca";
-
-function mostrarCarro() {
-  console.log(carro);
-}
-
-mostrarCarro();
-console.log(carro);
-
-var mes;
-
-if (false) {
-  const mes = "Dezembro";
-  console.log(mes);
-}
-
-// console.log(mes);
-
+// Por qual motivo o código abaixo retorna com erros?
 {
-  var carro = "Fusca";
-  const ano = 2018;
+  var cor = 'preto';
+  const marca = 'Fiat';
+  let portas = 4;
+  console.log(cor, marca, portas);
 }
-console.log(carro);
-console.log(ano);
+
+// Como corrigir o erro abaixo?
+const dois = 2;
+function somarDois(x) {
+  return x + dois;
+}
+function dividirDois(x) {
+  return x / dois;
+}
+console.log(somarDois(4));
+console.log(dividirDois(10));
+
+// O que fazer para total retornar 500?
+const numero = 50;
+
+for(let numero = 0; numero < 10; numero++) {
+  console.log(numero);
+}
+
+const total = 10 * numero;
+console.log(total);
