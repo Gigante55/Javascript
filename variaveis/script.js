be8f84b58b866472e4ddf900d724b0b6dfe704d8
Ponton_Silva <raphaelrodriguesteste@gmail.com>
2026-05-06 17:45:01 -0300

    aaqee

--- variaveis/script.js
+++ variaveis/script.js
@@ -1,46 +1,25 @@
-// Verifique a distância da primeira imagem
-// em relação ao topo da página
 const img = document.querySelector("img");
-const imgTop = img.offsetTop;
 
-console.log(imgTop);
-
-// Retorne a soma da largura de todas as imagens
-function somaImagens() {
-  const imagens = document.querySelectorAll("img");
-  let soma = 0;
-  imagens.forEach((imagem) => {
-    soma += imagem.offsetWidth;
-  });
-  console.log(soma);
+function callback(event) {
+  console.log(event);
 }
 
-window.onload = function () {
-  somaImagens();
-};
+img.addEventListener("click", callback);
 
-// Verifique se os links da página possuem
-// o mínimo recomendado para telas utilizadas
-// com o dedo. (48px/48px de acordo com o google)
+const animaisLista = document.querySelector(".animais-lista");
 
-const links = document.querySelectorAll("a");
-
-links.forEach((link) => {
-  const linkWidth = link.offsetWidth;
-  const linkHeight = link.offsetHeight;
-  if (linkWidth >= 48 && linkHeight >= 48) {
-    console.log(link, "Possui boa acessibilidade");
-  } else {
-    console.log(link, "Não possui boa acessibilidade");
-  }
-});
-
-// Se o browser for menor que 720px,
-// adicione a classe menu-mobile ao menu
-
-const browserSmall = window.matchMedia("(max-width: 720px)").matches;
-
-if (browserSmall) {
-  const menu = document.querySelector(".menu");
-  menu.classList.add("menu-mobile");
+function callbackLista(event) {
+  console.log(event.currentTarget);
+  console.log(event.target);
 }
+
+animaisLista.addEventListener("click", callbackLista);
+
+const linkExterno = document.querySelector('a[href^="http"]');
+
+function handleLinkExterno() {
+  event.preventDefault();
+  console.log(event);
+}
+
+linkExterno.addEventListener("click", handleLinkExterno);
