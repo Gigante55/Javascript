5c117e5b760af59feb715579697d08726882d63a
Ponton_Silva <raphaelrodriguesteste@gmail.com>
2026-05-15 17:13:10 -0300

    vbn

--- variaveis/script.js
+++ variaveis/script.js
@@ -6,3 +6,10 @@ copy.appendChild(cloneMenu);
 
 const faq = document.querySelector(".faq");
 const primeiroDt = faq.querySelector("dt");
+const proximoDD = primeiroDt.nextElementSibling;
+
+console.log(proximoDD);
+
+const animais = document.querySelector(".animais");
+
+faq.innerHTML = animais.innerHTML;
