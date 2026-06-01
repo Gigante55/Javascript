905f063014560c85b9495749ba32186201347ff5
Ponton_Silva <raphaelrodriguesteste@gmail.com>
2026-06-01 18:16:57 -0300

    asdasdsadsad

--- variaveis/Objetos/script.js
+++ variaveis/Objetos/script.js
@@ -13,3 +13,29 @@ function Pessoa(nome, idade) {
     console.log(nome + "Andou");
   };
 }
+
+const joao = new Pessoa("João", 20);
+const maria = new Pessoa("Maria", 25);
+const bruno = new Pessoa("Bruno", 15);
+
+function Dom(seletor) {
+  const elementList = document.querySelectorAll(seletor);
+  this.elements = elementList;
+  this.addClass = function (classe) {
+    elementList.forEach((element) => {
+      element.classList.add(classe);
+    });
+  };
+  this.removeClass = function (classe) {
+    elementList.forEach((element) => {
+      element.classList.remove(classe);
+    });
+  };
+}
+
+const listaItens = new Dom("li");
+const ul = new Dom("ul");
+
+listaItens.addClass("ativar");
+
+ul.addClass("ativar-ul");
