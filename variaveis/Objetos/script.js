f7094add42553a8ad1dd658838a2086351023fc4
Ponton_Silva <raphaelrodriguesteste@gmail.com>
2026-06-08 17:23:04 -0300

    asdadsasdasd

--- variaveis/Objetos/script.js
+++ variaveis/Objetos/script.js
@@ -1,9 +1,38 @@
-const comida = "Pizza";
-const agua = new String("Agua");
+// const comida = "Pizza";
+// const agua = new String("Agua");
 
-console.log(agua.length);
+// console.log(agua.length);
 
-const frase = "A melhor comida";
+// const frase = "A melhor comida";
 
-console.log(frase[frase.length - 1]);
-console.log(frase.charAt(frase.length - 1));
+// console.log(frase[frase.length]);
+// console.log(frase.charAt(frase.length));
+
+const frase = "A melhor linguagem é";
+const linguagem = "JavaScript";
+
+const fraseFinal = frase.concat(linguagem, "!!", "Oi");
+
+const fruta = "Banana";
+const listaFrutas = "Melancia, Banana, Laranja";
+
+console.log(listaFrutas.includes(listaFrutas));
+
+console.log(fruta.endsWith("na"));
+console.log(fruta);
+
+const transacao1 = "Depósito de cliente";
+const transacao2 = "Depósito de fornecedor";
+const transacao3 = "Taxa de camisas";
+
+console.log(transacao1.slice(-5));
+
+console.log(fruta.indexOf("na"));
+
+const preco = "R$ 99,00";
+
+const listaPrecos = ["R$ 99", "R$ 199", "R$ 12000"];
+
+listaPrecos.forEach((item) => {
+  console.log(item.padStart(10, "-"));
+});
