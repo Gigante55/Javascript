6ddc7422a891bea148af27680c0667ad127fcba3
Ponton_Silva <raphaelrodriguesteste@gmail.com>
2026-06-15 16:59:30 -0300

    asdadwadsdad

--- variaveis/Objetos/script.js
+++ variaveis/Objetos/script.js
@@ -1,78 +1,15 @@
-// Utilizando o foreach na array abaixo,
-// some os valores de Taxa e os valores de Recebimento
+console.log(Number.isNaN("ds"));
+console.log(Number.isNaN(10.4343434));
 
-const transacoes = [
-  {
-    descricao: "Taxa do Pão",
-    valor: "R$ 39",
-  },
-  {
-    descricao: "Taxa do Mercado",
-    valor: "R$ 129",
-  },
-  {
-    descricao: "Recebimento de Cliente",
-    valor: "R$ 99",
-  },
-  {
-    descricao: "Taxa do Banco",
-    valor: "R$ 129",
-  },
-  {
-    descricao: "Recebimento de Cliente",
-    valor: "R$ 49",
-  },
-];
+console.log(parseFloat("32434.4343"));
+console.log(parseFloat("100.27 reais"));
+console.log(parseInt("100.27 reais", 10));
+console.log(parseInt(23.49, 10));
 
-let taxaTotal = 0;
-let recebimentoTotal = 0;
-transacoes.forEach((item) => {
-  const numeroLimpo = +item.valor.replace("R$ ", "");
-  if (item.descricao.slice(0, 4) === "Taxa") {
-    taxaTotal += numeroLimpo;
-  } else if (item.descricao.slice(0, 4) === "Taxa") {
-    recebimentoTotal += numeroLimpo;
-  }
-});
-console.log(taxaTotal);
-console.log(recebimentoTotal);
+const preco = 10.32323;
+console.log(preco.toFixed());
+const valor = 48.49;
 
-// Retorne uma array com a lista abaixo
-const transportes = "Carro;Avião;Trem;Ônibus;Bicicleta";
-
-const arrayTransportes = transportes.split(";");
-console.log(arrayTransportes);
-
-// Substitua todos os span's por a's
-let html = `<ul>
-                <li><span>Sobre</span></li>
-                <li><span>Produtos</span></li>
-                <li><span>Contato</span></li>
-              </ul>`;
-
-html = html.split("span").join("a");
-
-console.log(html);
-
-// Retorne o último caracter da frase
-const frase = "Melhor do ano!";
-
-console.log(frase[frase.length - 1]);
-
-// Retorne o total de taxas
-const transacoes2 = [
-  "Taxa do Banco",
-  "   TAXA DO PÃO",
-  "  taxa do mercado",
-  "depósito Bancário",
-  "TARIFA especial",
-];
-
-let taxasTotal = 0;
-transacoes2.forEach((item) => {
-  item = item.toLowerCase().trim().slice(0, 4);
-
-  if (item === "taxa") taxasTotal++;
-});
-
-console.log(taxasTotal);
+let valor = 48.49;
+valor = valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+console.log(valor);
