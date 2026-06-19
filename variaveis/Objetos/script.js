d5f5d6a2dadde188e6e565201b1078704d6290b3
Ponton_Silva <raphaelrodriguesteste@gmail.com>
2026-06-19 15:25:07 -0300

    dasdasdasdasd

--- variaveis/Objetos/script.js
+++ variaveis/Objetos/script.js
@@ -1,38 +1,58 @@
-const instrumentos = ["Guitarra", "Baixo", "Violão"];
-const precos = [49, 99, 69, 89];
+// const instrumentos = ["Guitarra", "Baixo", "Violão"];
+// const precos = [49, 99, 69, 89];
 
-const dados = [
-  new String("Tipo 1"),
-  ["Carro", "Portas", { cor: "Azul", preco: 2000 }],
-  function andar(nome) {
-    console.log(nome);
-  },
-];
+// const dados = [
+// new String("Tipo 1"),
+// ["Carro", "Portas", { cor: "Azul", preco: 2000 }],
+// function andar(nome) {
+// console.log(nome);
+// },
+// ];
 
-const carros = new Array("Ford", "Fiat", "Honda");
+// const carros = new Array("Ford", "Fiat", "Honda");
 
-carros[2] = "Ferrari";
-carros[3] = "Kia";
-carros[20] = "Kia";
+// carros[2] = "Ferrari";
+// carros[3] = "Kia";
+// carros[20] = "Kia";
 
-console.log(carros.length);
+// console.log(carros.length);
 
-const li = document.querySelectorAll("li");
+// const li = document.querySelectorAll("li");
 
-const arrayLi = Array.from(li);
+// const arrayLi = Array.from(li);
 
-const obj = {
-  0: "Andre",
-  1: "Rafael",
-  2: "Teste",
-  length: 4,
-};
+// const obj = {
+// 0: "Andre",
+// 1: "Rafael",
+// 2: "Teste",
+// length: 4,
+// };
 
-const objArray = Array.from(obj);
+// const objArray = Array.from(obj);
 
-console.log(li);
-console.log(arrayLi);
+// console.log(li);
+// console.log(arrayLi);
 
-const frutas = ["Banana", "Pêra", ["Uva Roxa", "Uva Verde"]];
+// const frutas = ["Banana", "Pêra", ["Uva Roxa", "Uva Verde"]];
 
-console.log(frutas.length);
+// console.log(frutas[2][0].length);
+
+// const instrumento = ["Guitarra", "Baixo", "Violão"];
+// instrumentos.sort();
+
+// const idade = [32, 21, 33, 43, 1, 12, 8];
+// idades.sort();
+
+// console.log(instrumentos);console.log(idades);
+
+const carros = ["Ford", "Fiat", "VW"];
+carros.unshift("Kia", "Ferrari");
+const novaArray = carros.push("Parati", "Gol");
+
+console.log(novaArray);
+
+console.log(carros);
+
+console.log(carros.shift());
+
+console.log(carros);
