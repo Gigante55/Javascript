8292711c8ae0f56c927e28dc05658205d1641e0d
Ponton_Silva <raphaelrodriguesteste@gmail.com>
2026-06-22 17:30:19 -0300

    nmnb

--- variaveis/Objetos/script.js
+++ variaveis/Objetos/script.js
@@ -59,3 +59,17 @@ console.log(carros);
 console.log(carros.splice(2, 0, "Fusca"));
 
 console.log(carros);
+
+// console.log(["Item1", "Item2", "Item3", "Item4", "Item5"].copyWithin(2, 0, 2));
+
+console.log(["Item1", "Item2", "Item3", "Item4"].fill("Banana", 0, 3));
+
+const transporte1 = ["Barco", "Aviao"];
+const transporte2 = ["Carro", "Moto"];
+
+const linguagens = ["html", "css", "js", "php", "python", "js"];
+
+let htmlString = "<h2>Título Principal</h2>";
+htmlString = htmlString.split("h2");
+
+console.log(htmlString);
