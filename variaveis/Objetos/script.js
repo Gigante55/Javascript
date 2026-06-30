67ec41c3db04916d5958c54cf49f10a01e372ab9
Ponton_Silva <raphaelrodriguesteste@gmail.com>
2026-06-30 14:22:15 -0300

    adasdasdasd

--- variaveis/Objetos/script.js
+++ variaveis/Objetos/script.js
@@ -1,47 +1,22 @@
-const comidas = ["Pizza", "Frango", "Carne", "Macarrão"];
-// Remova o primeiro valor de comidas e coloque em uma variável
-// Remova o último valor de comidas e coloque em uma variável
-// Adicione 'Arroz' ao final da array
-// Adicione 'Peixe' e 'Batata' ao início da array
+// const carros = ["Ford", "Fiat", "Honda"];
 
-const primeiroValor = comidas.shift();
-const ultimoValor = comidas.pop();
+// carros.forEach((item, index, array) => {
+//   array[index] = "Teste";
+//   console.log(item.toUpperCase(), index, array);
+// });
 
-comidas.push("Arroz");
-comidas.unshift("Peixe", "Batata");
+// console.log(carros);
 
-console.log(primeiroValor);
-console.log(ultimoValor);
-console.log(comidas);
+// const li = document.querySelectorAll("li");
 
-const estudantes = ["Marcio", "Brenda", "Joana", "Kleber", "Julia"];
-// Arrume os estudantes em ordem alfabética
-// Inverta a ordem dos estudantes
-// Verifique se Joana faz parte dos estudantes
-// Verifique se Juliana faz parte dos estudantes
-estudantes.sort();
-estudantes.reverse();
+// const retornoForeach = li.forEach((item, index) => {
+//   item.classList.add("ativa" + index);
+// });
 
-console.log(estudantes);
-console.log(estudantes.includes("Joana"));
-console.log(estudantes.includes("Juliana"));
+// console.log(retornoForeach);
 
-let html = `<section>
-              <div>Sobre</div>
-              <div>Produtos</div>
-              <div>Contato</div>
-            </section>`;
-// Substitua section por ul e div com li,
-// utilizando split e join
-html = html.split("section").join("ul").split("div").join("li");
-console.log(html);
+const carros = ["Ford", "Fiat", "Honda"];
 
-const carros = ["Ford", "Fiat", "VW", "Honda"];
-// Remova o último carro, mas antes de remover
-// salve a array original em outra variável
-const carrosCopia = carros.slice();
-
-carros.pop();
-
-console.log(carros);
-console.log(carrosCopia);
+carros.map((item, index, array) => {
+  console.log(item, index, array);
+});
