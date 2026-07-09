d104bb2b7797cbaadc632d0f57d1cfc64ace89e7
Ponton_Silva <raphaelrodriguesteste@gmail.com>
2026-07-09 17:52:30 -0300

    asadsad

--- variaveis/Objetos/script.js
+++ variaveis/Objetos/script.js
@@ -1,14 +1,43 @@
-const perimetro = new Function('lado', 'return lado * 4');
+// const perimetro = new Function('lado', 'return lado * 4');
 
 
-function somar(n1, n2, n4) {
-  return n1 + n2;
+// function somar(n1, n2, n4) {
+//   return n1 + n2;
+// }
+
+// console.log(somar.name)
+
+// function darOi(nome, idade) {
+//   console.log('Oi para você' + nome + idade)
+// }
+
+// darOi.call(null, 'Andre', 28);
+
+// window.marca = 'Carro';
+// window.ano = 288
+
+// function descricaoCarro(velocidade) {
+//   console.log(this)
+//   console.log(this.marca + ' ' + this.ano + velocidade);
+// }
+
+// descricaoCarro.call({marca: 'Honda', ano: 2015}, 100);
+
+const carros = ['Ford', 'Fiat', 'VW']
+const frutas = ['Banana', 'Uva', 'Pêra']
+
+carros.forEach.call(frutas, (item) => {
+  console.log(item)
+})
+
+function Dom(seletor) {
+  this.element = document.querySelector(seletor)
 }
 
-console.log(somar.name)
-
-function darOi(nome) {
-  console.log('Oi para você' = nome)
+Dom.prototype.ativo = function(classe) {
+this.element.classList.add(classe)
 }
 
-darOi.call({}, 'Andre');
+const ul = new Dom('ul');
+
+console.log(ul.element);
