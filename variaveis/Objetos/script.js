dd6406239ef0ceb22852570a4bd885973e0d0514
Ponton_Silva <raphaelrodriguesteste@gmail.com>
2026-07-14 16:45:00 -0300

    asdasdasdasd

--- variaveis/Objetos/script.js
+++ variaveis/Objetos/script.js
@@ -1,102 +1,27 @@
-// const perimetro = new Function('lado', 'return lado * 4');
+// Retorne a soma total de caracteres dos
+// parágrafos acima utilizando reduce
+const paragrafos = document.querySelectorAll('p');
 
+const totalCaracteres = Array.prototype.reduce.call(paragrafos, (acumulador, item) => {
+return acumulador + item.innerText.length;
+}, 0)
 
-// function somar(n1, n2, n4) {
-//   return n1 + n2;
-// }
+console.log(totalCaracteres)
+console.log(paragrafos)
 
-// console.log(somar.name)
-
-// function darOi(nome, idade) {
-//   console.log('Oi para você ' + nome + idade)
-// }
-
-// darOi.call(null, 'André', 28);
-
-// window.marca = 'Carro';
-// window.ano = 288
-
-// function descricaoCarro(velocidade) {
-//   console.log(this)
-//   console.log(this.marca + ' ' + this.ano + velocidade);
-// }
-
-// descricaoCarro.call({marca: 'Honda', ano: 2015}, 100);
-
-// const carros = ['Ford', 'Fiat', 'VW'];
-// const frutas = ['Banana', 'Uva', 'Pêra'];
-
-
-// carros.forEach.call(frutas, (item) => {
-//   console.log(item)
-// })
-
-// function Dom(seletor) {
-//   this.element = document.querySelector(seletor);
-// }
-
-// Dom.prototype.ativo = function(classe) {
-//   console.log(this);
-//   this.element.classList.add(classe);
-// }
-
-// const li = {
-//   element: document.querySelector('li')
-// }
-
-// Dom.prototype.ativo.call(li, 'ativar');
-
-
-// const ul = new Dom('ul');
-
-
-
-
-// ul.ativo.call(li, 'ativo');
-// ul.ativo('ativar');
-
-const frutas = ['Uva', 'Maçã', 'Banana'];
-
-Array.prototype.pop.call(frutas);
-frutas.pop();
-
-const arrayLike = {
-  0: 'Item 1',
-  1: 'Item 2',
-  2: 'Item 3',
-  length: 3,
+// Crie uma função que retorne novos elementos
+// html, com os seguintes parâmetros
+// tag, classe e conteudo.
+function criarElemento(tag, classe, conteudo) {
+  const elemento = document.createElement(tag);
+classe ? elemento.classList.add(classe) : null;
+conteudo ? elemento.innerHTML = conteudo : null;
+  return elemento;
 }
 
-const li = document.querySelectorAll('li');
-const arrayLi = Array.from(li);
-
-const filtro = arrayLi.filter((item) => {
-  return item.classList.contains('ativo');
-})
-
-console.log(filtro);
-
-console.log(li)
-
-const numeros = [33, 23, 2, 42314, 234, 234, 34, 243]
-
-const $ = document.querySelectorAll.bind(document);
+console.log(criarElemento('li', 'ativa'))
 
 
-const carro = {
-  marca: 'Ford',
-  ano: 2018,
-  acelerar: function(aceleracao, tempo) {
-    return `${this.marca} acelerou ${aceleracao} em ${tempo}`;
-  }
-}
-
-const honda = {
-marca: 'Honda'
-}
-
-const acelerarHonda = carro.acelerar.bind(honda)
-
-console.log(acelerarHonda(30))
-
-console.log(carro)
+// Crie uma nova função utilizando a anterior como base
+// essa nova função deverá sempre criar h1 com a
+// classe titulo. Porém o parâmetro conteudo continuará dinâmico
