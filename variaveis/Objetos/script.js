f1c4e96f0bb02d6985fa814a5fda6870c7fa6df6
Ponton_Silva <raphaelrodriguesteste@gmail.com>
2026-07-16 15:09:12 -0300

    asdasdasdasd

--- variaveis/Objetos/script.js
+++ variaveis/Objetos/script.js
@@ -1,19 +1,60 @@
-const pessoa = new Object({
-  nome: 'André'
-})
+// const pessoa = new Object({
+//   nome: 'André'
+// })
 
-console.log(pessoa)
+// console.log(pessoa)
 
-const carro = {
-  marca: 'Marca',
-  rodas: 4,
-  acelerar() {
-    return this.marca + ' acelerou';
-  },
-  buzinar() {
-    return this.marca + ' buzinou';
-  }
+// const carro = {
+//   rodas: 4,
+//   init(valor) {
+// this.marca = valor;
+// return valor;
+//   },
+//   acelerar() {
+//     return this.marca + ' acelerou';
+//   },
+//   buzinar() {
+//     return this.marca + ' buzinou';
+//   }
+// }
+
+// const honda = Object.create(carro).init('Hondas');
+// console.log(honda.acelerar());
+
+// const ferrari = Object.create(carro).init('Ferrari');
+// console.log(ferrari.acelerar());
+
+
+// const funcaoAutomovel = {
+//   acelerar() {
+//     return 'acelerou';
+//   },
+//   buzinar() {
+//     return 'buzinou';
+//   },
+// }
+
+// const moto = {
+//   rodas: 2,
+//   capacete: true,
+// }
+
+// Object.assign(moto, funcaoAutomovel, carro);
+
+// console.log(moto)
+
+const moto = {
+  capacete: true,
 }
 
-const honda = Object.create(carro);
-honda.marca = 'Honda';
+
+Object.defineProperties(moto, {
+rodas: {
+  value: 2,
+  configurable: false,
+  writable: true,
+}
+})
+
+moto.rodas;
+console.log(moto)
