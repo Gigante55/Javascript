e48d0f8d0b9775d6f2331789f44e65effd53240f
Ponton_Silva <raphaelrodriguesteste@gmail.com>
2026-07-20 16:25:32 -0300

    asdasdasdasd

--- variaveis/Objetos/script.js
+++ variaveis/Objetos/script.js
@@ -1,111 +1,18 @@
-// const pessoa = new Object({
-//   nome: 'André'
-// })
+// Crie uma função que verifique
+// corretamente o tipo de dado
 
-// console.log(pessoa)
+// Crie um objeto quadrado com
+// a propriedade lados e torne
+// ela imutável
 
-// const carro = {
-//   rodas: 4,
-//   init(valor) {
-// this.marca = valor;
-// return valor;
-//   },
-//   acelerar() {
-//     return this.marca + ' acelerou';
-//   },
-//   buzinar() {
-//     return this.marca + ' buzinou';
-//   }
-// }
-
-// const honda = Object.create(carro).init('Hondas');
-// console.log(honda.acelerar());
-
-// const ferrari = Object.create(carro).init('Ferrari');
-// console.log(ferrari.acelerar());
-
-
-// const funcaoAutomovel = {
-//   acelerar() {
-//     return 'acelerou';
-//   },
-//   buzinar() {
-//     return 'buzinou';
-//   },
-// }
-
-// const moto = {
-//   rodas: 2,
-//   capacete: true,
-// }
-
-// Object.assign(moto, funcaoAutomovel, carro);
-
-// console.log(moto)
-
-// const moto = {
-//   capacete: true,
-// }
-
-
-// Object.defineProperties(moto, {
-// rodas: {
-//   enumerable: true,
-// get() {
-// return this._rodas;
-// },
-// set(valor) {
-// this._rodas = valor * 4 + 'Total Rodas'
-// }
-// }
-// })
-
-// console.log(moto)
-
-// const innerHeightConfig = Object.getOwnPropertyDescriptor(window, 'innerHeight');
-
-// Object.getOwnPropertyNames(Array.prototype)
-
-// const frutas = ['Banana'];
-
-// console.log(Object.getPrototypeOf(''));
-// console.log(Array.prototype);
-
-// const frutas1 = ['Banana', 'Pêra'];
-// const frutas2 = ['Banana', 'Pêra']
-
-// const novaFruta = frutas1;
-
-// novaFruta[0] = 'Uva';
-
-// console.log(frutas1)
-
-// Object.is(frutas1, novaFruta)
-
-// const carro = {
-//   marca: 'Ford',
-//   ano: 2018,
-// }
-
-// Object.freeze(carro);
-// carro.portas = 4;
-// delete carro.marca;
-
-// carro.marca = 'Honda'
-
-// console.log(carro)
-// console.log(Object.isFrozen(carro))
-
-const frutas = ['Banana', 'Uva'];
-const frase = 'Oi frase';
-const somar = function() {
-  return a + b;
-};
-
-const carro = {
-  marca: 'Ford'
+// Previna qualquer mudança
+// no objeto abaixo
+const configuracao = {
+  width: 800,
+  height: 600,
+  background: '#333'
 }
 
-console.log(frutas.toString())
-
-console.log(Object.prototype.toString.call(frutas));
+// Liste o nome de todas
+// as propriedades do
+// protótipo de String e Array
