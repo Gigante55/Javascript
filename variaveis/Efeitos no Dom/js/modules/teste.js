3ffb59e61ebfa2b0f30c8efda66361b0f88bd3fe
Ponton_Silva <raphaelrodriguesteste@gmail.com>
2026-07-27 17:53:06 -0300

    sdhsjndjhsaddsadjhujhsadjhsdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- variaveis/Efeitos no Dom/js/modules/teste.js
+++ variaveis/Efeitos no Dom/js/modules/teste.js
@@ -1,3 +1,9 @@
-export default function testando(){
+export function teste1(){
   console.log('Isso é teste')
 }
+
+export function teste2(){
+  console.log('Isso é teste skjdljks')
+}
+
+export const senha = 25102012;
--- variaveis/Efeitos no Dom/js/script.js
+++ variaveis/Efeitos no Dom/js/script.js
@@ -1,11 +1,12 @@
-import initScrollSuave from './modules/scroll-suave.js'
-import initAnimacaoScroll from './modules/scroll-animacao.js'
-import initAccordion from './modules/accordion.js'
-import initTabNav from './modules/tabnav.js'
-import teste from './modules/teste.js'
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+import * as teste from './modules/teste.js';
 
-
-teste();
+teste.teste1()
+teste.teste2()
+console.log();
 
 initScrollSuave();
 initAnimacaoScroll();
