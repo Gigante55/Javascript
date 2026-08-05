1a5b11f7c77ae2ca8426894db053cd0e2aa9c8d5
Ponton_Silva <raphaelrodriguesteste@gmail.com>
2026-08-05 17:46:32 -0300

    asdasdasdasd

--- variaveis/Efeitos no Dom/js/script.js
+++ variaveis/Efeitos no Dom/js/script.js
@@ -4,7 +4,6 @@ import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 
-
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
