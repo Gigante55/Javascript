3faa75788ccca0ce882ba01d1ab7b9e4b92319d3
Ponton_Silva <raphaelrodriguesteste@gmail.com>
2026-08-05 18:02:56 -0300

    asddddddddddddddddddddddddddddddddddddddd

---
+++ variaveis/Efeitos no Dom/js/modules/tooltip.js
@@ -0,0 +1,9 @@
+export default function initTooltip() {
+  
+}
+
+const tooltips = document.querySelectorAll('[data-tooltip]')
+
+tooltips.forEach((item) => {
+  item.addEventListener('mouseover', onMouseOver);
+})
