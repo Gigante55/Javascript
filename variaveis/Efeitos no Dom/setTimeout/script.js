da9f9bd4f9ce0a3294c7007ab30536f25b7dd436
Ponton_Silva <raphaelrodriguesteste@gmail.com>
2026-08-20 17:37:08 -0300

    '174

--- variaveis/Efeitos no Dom/setTimeout/script.js
+++ variaveis/Efeitos no Dom/setTimeout/script.js
@@ -2,6 +2,13 @@ const btn = document.querySelector('button');
 
 btn.addEventListener('click', handleClick);
 
+function handleClick() {
+  setTimeout(() => {
+    console.log(this)
+    this.classList.add('active');
+  }, 1000)
+}
+
 setTimeout(function() {
 console.log('Testando')
 }, 0);
