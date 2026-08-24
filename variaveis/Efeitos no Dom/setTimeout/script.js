76e109b8be7fbd63a8755d27e7044ea11128d64f
Ponton_Silva <raphaelrodriguesteste@gmail.com>
2026-08-24 18:38:11 -0300

    afe

--- variaveis/Efeitos no Dom/setTimeout/script.js
+++
@@ -1,7 +0,0 @@
-// Mude a cor da tela para azul e depois para vermelho a cada 2s.
-function mudarClasse() {
-  document.body.classList.toggle('active');
-}
-
-// Crie um cronometro utilizando o setInterval. Deve ser possível
-// iniciar, pausar e resetar (duplo clique no pausar).
