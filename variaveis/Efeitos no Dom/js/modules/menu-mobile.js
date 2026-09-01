133118841b4fc35b6c4bd711c2ede82c0bb57b03
Ponton_Silva <raphaelrodriguesteste@gmail.com>
2026-09-01 18:02:09 -0300

    44412214214

--- variaveis/Efeitos no Dom/js/modules/menu-mobile.js
+++ variaveis/Efeitos no Dom/js/modules/menu-mobile.js
@@ -6,14 +6,17 @@ export default function initMenuMobile() {
 
 const menuButton = document.querySelector('[data-menu="button"]');
 const menuList = document.querySelector('[data-menu="list"]');
+const eventos = ['click', 'touchstart'];
 
 function openMenu(event) {
 menuList.classList.add('active');
 menuButton.classList.add('active');
-outsideClick(menuList, ['click', 'touchstart'], () => {
+outsideClick(menuList, eventos, () => {
+  console.log('teste')
+
 menuList.classList.remove('active');
 menuButton.classList.remove('active');
 })
 }
 
-menuButton.addEventListener('click', openMenu);
+eventos.forEach(evento => menuButton.addEventListener(evento, openMenu))
