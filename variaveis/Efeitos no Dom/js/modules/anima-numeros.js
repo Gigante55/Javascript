850bc3b6aac820915c5bfb64f08acdc85815d4d2
Ponton_Silva <raphaelrodriguesteste@gmail.com>
2026-09-09 17:01:06 -0300

    68r4

--- variaveis/Efeitos no Dom/js/modules/anima-numeros.js
+++ variaveis/Efeitos no Dom/js/modules/anima-numeros.js
@@ -6,5 +6,13 @@ const numeros = document.querySelectorAll('[data-numero]');
 
 numeros.forEach((numero) => {
   const total = +numero.innerText;
+  const inscremento = total / 100;
+  let start = 0;
+  const timer = setInterval(() => {
+    start = start + inscremento;
+    numero.innerText = start;
+    if(start > total)
+      clearInterval(timer)
+  }, 25)
   console.log(total)
 })
