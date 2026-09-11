8deb8b2790639adaac3e98d488fe02636b49ee0e
Ponton_Silva <raphaelrodriguesteste@gmail.com>
2026-09-11 17:45:04 -0300

    safsf

--- variaveis/Efeitos no Dom/js/modules/anima-numeros.js
+++ variaveis/Efeitos no Dom/js/modules/anima-numeros.js
@@ -2,7 +2,7 @@ export default function initAnimaNumeros() {
 
 }
 
-const numeros = document.querySelectorAll('[data-numero]');
+const numeros = document.querySelectorAll('[data-numero]');   
 
 numeros.forEach((numero) => {
   const total = +numero.innerText;
