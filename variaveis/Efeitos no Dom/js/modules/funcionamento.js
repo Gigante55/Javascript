d80680eabe54f16c11312d3672fb1063602ca1f2
Ponton_Silva <raphaelrodriguesteste@gmail.com>
2026-09-23 20:02:18 -0300

    462222222222222

--- variaveis/Efeitos no Dom/js/modules/funcionamento.js
+++ variaveis/Efeitos no Dom/js/modules/funcionamento.js
@@ -3,5 +3,17 @@ export default function initFuncionamento() {
 }
 
 const agora = new Date();
+const futuro = new Date('Dec 24 2018 23:59');
 
-console.log(agora.getDay())
+console.log(agora.getMonth());
+
+console.log(futuro)
+
+function transformarDias(tempo) {
+  return tempo / (24 * 60 * 60 * 1000);
+}
+
+const diasAgora = transformarDias(agora.getTime());
+const diasFuturo = transformarDias(futuro.getTime());
+
+console.log(diasFuturo - diasAgora);
